6234bb0f90d84012cc0aa1b79691aa974a9f0285
oz <ozmerchavy2@gmail.com>
2024-08-03 19:44:02 -0230

    cool

--- strategy.py
+++ strategy.py
@@ -1,5 +1,37 @@
 import random
 from engine import Game, KNOWN_WORDS, compare_words
+from collections import defaultdict
+import statistics
+
+
+def splits(guess, candidates):
+  """ sizes of groups that yield the same pattern """
+  groups = defaultdict(int)
+  
+  for candidate in candidates:
+    result = compare_words(guess, candidate)
+    groups[result] += 1
+    
+  return sorted(groups.values())
+
+
+def best_candidate(candidates):
+  if len(candidates) == 1:
+    return candidates[0]
+      
+  best_guess = None
+  best_score = float("inf")
+  
+  list = KNOWN_WORDS if len(candidates) > 10 else candidates
+  
+  for guess in list:
+    score = statistics.geometric_mean(splits(guess, candidates))
+    
+    if score < best_score:
+      best_score = score
+      best_guess = guess
+      
+  return best_guess
 
 
 def play(game: Game) -> str:
@@ -9,7 +41,7 @@ def play(game: Game) -> str:
   candidates = KNOWN_WORDS
   
   while True:
-    guess = random.choice(candidates)
+    guess = best_candidate(candidates)
     result_pattern = game.make_a_guess(guess)
     
     if result_pattern == "🟩🟩🟩🟩🟩":
